064442248820edfbbf0af2c1d37edac2c3b62a94
Carolyn Perniciaro <carolynmperniciaro@gmail.com>
2022-07-22 10:35:15 -0500

    removed comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,14 +10,14 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'intelligems-io', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'intelligems-io',
+  projectName: 'docs',
   themeConfig: {
 
     navbar: {
       title: 'Intelligems',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Intelligems Logo',
         src: 'img/icon_color_512.png',
       },
       items: [
@@ -27,12 +27,6 @@ module.exports = {
           position: 'left',
           label: 'Documentation',
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
-        // {
-        //   href: 'https://github.com/facebook/docusaurus',
-        //   label: 'GitHub',
-        //   position: 'right',
-        // },
       ],
     },
     footer: {
@@ -52,16 +46,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // editUrl:
-          //   'https://github.com/intelligems-io/docusaurus/edit/master/website/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
